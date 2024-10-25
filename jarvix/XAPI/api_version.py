57ee50e84781315102cfea2b88b81cce977e38de
Serge MUGISHA <mugishasergebonheur@gmail.com>
2024-10-25 02:26:20 -0400

    refactor API version code

--- jarvix/XAPI/api_version.py
+++ jarvix/XAPI/api_version.py
@@ -1,7 +1,27 @@
+import os
+from enum import Enum
+
 import openai
 from anthropic import Anthropic
-
 from pydantic import BaseModel, Field
+
+CHAT_ROLE_MESSAGE = (
+    "You are an AI System Called Jarvix. Your job is to answer every question users ask you. "
+    "Don't forget your name is Jarvix. If a user asks, please tell them your name. You only speak ENGLISH."
+)
+
+IOT_ROLE_MESSAGE = (
+    "You are a helpful assistant that extracts actions and entity names from user commands for use in home automation API."
+)
+
+class ModelType(Enum):
+    GPT = "GPT"
+    CLAUDE = "CLAUDE"
+    OLLAMA = "OLLAMA"
+
+class ChatType(Enum):
+    CONVERSATION = "conversation"
+    IOT = "iot"
 
 class ApiClient(BaseModel):
     gpt_api_key: str = Field(..., env='OPENAI_API_KEY')
@@ -13,25 +33,29 @@ class ApiClient(BaseModel):
         protected_namespaces = ()
         arbitrary_types_allowed = True
 
-    def process_text_with_gpt(self, text: str) -> str:
-        client = openai.Client(api_key=self.gpt_api_key)
-        completion = client.chat.completions.create(
-            model=self.gpt_model_name,
-            messages=[
-                {"role": "system", "content": "You are an AI System Called Jarvix. Your Job is to answer every question users ask you. Don't forget your name is Jarvix. If a user asks please tell them your name. You only speak ENGLISH"},
-                {"role": "user", "content": text}
-            ]
-        )
-        return completion.choices[0].message.content
+    def process_text(self, text: str, chat_type: ChatType = ChatType.CONVERSATION) -> str:
+        selected_model = ModelType(os.getenv('SELECTED_MODEL'))
 
-    def process_text_with_claude(self, text: str) -> str:
-        client = Anthropic(api_key=self.claude_api_key)
-        completion = client.messages.create(
-            model=self.claude_model_name,
-            max_tokens=1000,
-            messages=[
-                {"role": "system", "content": "You are an AI System Called Jarvix. Your Job is to answer every question users ask you. Don't forget your name is Jarvix. If a user asks please tell them your name. You only speak ENGLISH"},
-                {"role": "user", "content": text}
-            ]
-        )
-        return completion.content[0].text
+        if selected_model == ModelType.GPT:
+            client = openai.Client(api_key=self.gpt_api_key)
+            completion = client.chat.completions.create(
+                model=self.gpt_model_name,
+                messages=[
+                    {"role": "system", "content": CHAT_ROLE_MESSAGE if chat_type == ChatType.CONVERSATION else IOT_ROLE_MESSAGE},
+                    {"role": "user", "content": text}
+                ]
+            )
+            return completion.choices[0].message.content
+        elif selected_model == ModelType.CLAUDE:
+            client = Anthropic(api_key=self.claude_api_key)
+            completion = client.messages.create(
+                model=self.claude_model_name,
+                max_tokens=1000,
+                messages=[
+                    {"role": "system", "content": CHAT_ROLE_MESSAGE if chat_type == ChatType.CONVERSATION else IOT_ROLE_MESSAGE},
+                    {"role": "user", "content": text}
+                ]
+            )
+            return completion.content[0].text
+        else:
+            raise ValueError("Unsupported model type. Use 'gpt' or 'claude'.")
